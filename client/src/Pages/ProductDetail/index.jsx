be3482413c4281dba588525a38ac0f17730624b1
Kawaljeet Singh Batra <batrakawaljeetsingh@gmail.com>
2021-04-20 20:47:52 +0530

    edited the product detail page

--- client/src/Pages/ProductDetail/index.jsx
+++ client/src/Pages/ProductDetail/index.jsx
@@ -1,17 +1,33 @@
-import React from 'react'
+import React from "react";
 import ProductDetailComp from "../../Components/ProductDetail";
 import MiniCart from "../../Components/MiniCart";
 import "./ProductDetailpage.css";
+import ProductCard from "../../Components/ProductCard";
 
-const ProductDetail = ({DisplayImages , ProductInfo }) => {
-    return (
-        <div className = "ProductDetailPage">
-            <ProductDetailComp
-           DisplayImages = {DisplayImages}
-            ProductInfo = {ProductInfo}
-            />
+const ProductDetailPage = ({ DisplayImages, ProductInfo }) => {
+  return (
+    <div className="ProductDetailPage">
+      <ProductDetailComp
+        DisplayImages={DisplayImages}
+        ProductInfo={ProductInfo}
+      />
+      <div className="Similar-products">
+        <p>SIMILAR PRODUCTS</p>
+        <div>
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default ProductDetail
+export default ProductDetailPage;
